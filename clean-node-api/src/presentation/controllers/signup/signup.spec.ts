6eaf8501f5ba95e775d20f3cfae9b86a81aa81ca
eleazar.nascimento-at-183685660734 <eleazar.nascimento@2share.app>
2023-08-13 23:57:22 -0300

    test: ensure SignUpController return 200 if valid data is provided

--- clean-node-api/src/presentation/controllers/signup/signup.spec.ts
+++ clean-node-api/src/presentation/controllers/signup/signup.spec.ts
@@ -163,8 +163,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password'
       }
     }
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(200);
+    sut.handle(httpRequest);
     expect(addSpy).toHaveBeenCalledWith({
       name: 'any_name',
       email: 'any_email@mail.com',
@@ -208,5 +207,26 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    });
   });
 });
